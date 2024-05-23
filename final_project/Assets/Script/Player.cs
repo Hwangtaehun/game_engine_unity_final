06065f948372e33b18060def439f20bb4b2241d9
Hwangtaehun <r283@naver.com>
2024-05-23 20:40:56 +0900

    UI 설계

--- final_project/Assets/Script/Player.cs
+++ final_project/Assets/Script/Player.cs
@@ -2,25 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine.SceneManagement;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour
 {
     [SerializeField] private float jumpPower;
+    private string timerText;
+    private float curTime = 0.0f;
+    GUIStyle style = new GUIStyle();
 
     void Start()
     {
-        
+        style.fontSize = 40;
+        style.normal.textColor = Color.white;
     }
 
     void Update()
     {
+        curTime += Time.deltaTime;
+
         if (Input.GetMouseButtonDown(0))
         {
             GetComponent<Rigidbody>().velocity = new Vector3(0, jumpPower, 0);
         }
     }
 
-    private void OnCollisionEnter(Collision collision)
+    void OnCollisionEnter(Collision collision)
     {
         if (collision.collider.tag == "Obstacle" || collision.collider.tag == "Wall")
         {
@@ -30,5 +37,12 @@ public class Player : MonoBehaviour
         {
             Destroy(collision.gameObject);
         }
+    }
+
+    void OnGUI()
+    {
+        string timerText = "½Ã°£ : " + curTime;
+        Rect textPos = new Rect(250, 250, 300, 60);
+        GUI.Label(textPos, timerText, style);
     }
 }
