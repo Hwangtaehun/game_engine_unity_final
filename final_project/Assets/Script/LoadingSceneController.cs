15374c04bec670fc7b79fc425d0de52d2e981d3b
Hwangtaehun <r283@naver.com>
2024-06-19 21:26:33 +0900

    로딩 화면 진행막대기 수정

--- final_project/Assets/Script/LoadingSceneController.cs
+++ final_project/Assets/Script/LoadingSceneController.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -8,6 +9,7 @@ public class LoadingSceneController : MonoBehaviour
 {
     static string nextScene;
     [SerializeField] Image progressBar;
+    [SerializeField] TMP_Text progresstext;
 
     public static void LoadScene(string sceneflame)
     {
@@ -26,6 +28,7 @@ public class LoadingSceneController : MonoBehaviour
         op.allowSceneActivation = false;
 
         float timer = 0.0f;
+        float percent;
 
         while (!op.isDone)
         {
@@ -34,11 +37,15 @@ public class LoadingSceneController : MonoBehaviour
             if (op.progress < 0.9f)
             {
                 progressBar.fillAmount = op.progress;
+                percent = op.progress * 100;
+                progresstext.text = percent.ToString("F2") + "%";
             }
             else
             {
                 timer += Time.unscaledDeltaTime;
                 progressBar.fillAmount = Mathf.Lerp(0.9f, 1.0f, timer);
+                percent = Mathf.Lerp(0.9f, 1.0f, timer) * 100;
+                progresstext.text = percent.ToString("F2") + "%";
 
                 if (progressBar.fillAmount >= 1.0f)
                 {
